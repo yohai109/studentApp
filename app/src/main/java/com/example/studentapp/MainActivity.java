82eb3c5530e8aebebf0a492cd9ade592e81b33ae
emilyzborovsky <emilyz@upstream.auto>
2022-01-16 23:34:29 +0200

    working nav drawer

--- app/src/main/java/com/example/studentapp/MainActivity.java
+++ app/src/main/java/com/example/studentapp/MainActivity.java
@@ -1,18 +1,33 @@
 package com.example.studentapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.NavHost;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.studentapp.viewmodels.StudentListViewModel;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     NavController navController;
     private StudentListViewModel viewModel;
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +42,37 @@ public class MainActivity extends AppCompatActivity {
                 this,
                 ((StudentApplication) getApplication()).getFactory()
         ).get(StudentListViewModel.class);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).setDrawerLayout(drawer).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                                                             @Override
+                                                             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                                                                 if (item.getItemId() == R.id.nav_add) {
+                                                                     navController.navigate(StudentListRvFragmentDirections.actionStudentListRvFragmentToNewStudentFragment());
+                                                                 }
+                                                                 drawer.close();
+                                                                 return false;
+                                                             }
+
+                                                         }
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
